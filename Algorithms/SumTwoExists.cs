f0ce5abde6820f2e1aebec4f277862870c8dbd35
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-09 11:28:53 +1000

    Dictionary for Sum

--- Algorithms/SumTwoExists.cs
+++ Algorithms/SumTwoExists.cs
@@ -1,8 +1,22 @@
-﻿namespace Algorithms
+﻿using System.Collections.Generic;
+
+namespace Algorithms
 {
     public static class SumTwoExists
     {
-        public static bool Run(int[] numbers, int sum)
+        public static bool Run(int[] numbers, int sum) /* O(n) */
+        {
+            var set = new HashSet<int>();
+            foreach (var number in numbers)
+            {
+                if (set.Contains(sum - number))
+                    return true;
+                set.Add(number);
+            }
+            return false;
+        }
+
+        public static bool RunBinarySearch(int[] numbers, int sum) /* O(log(n)) */
         {
             QuickSort.Sort(numbers);
             for (int i = 0; i < numbers.Length; i++)
