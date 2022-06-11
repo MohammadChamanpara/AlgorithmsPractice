8136046dcea04fd44addc7a7ddf18394ee78eb12
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-11 10:41:19 +1000

    Find Missing number

---
+++ Algorithms.Tests/MissingNumberTests.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class MissingNumberTests
+    {
+        [Theory]
+        [InlineData(new[] { 3, 1 }, 2)]
+        [InlineData(new[] { 1, 3, 5, 2 }, 4)]
+        [InlineData(new[] { 1, 2, 4, 5, 6, 3, 8 }, 7)]
+        public void Test(int[] numbers, int expectedMissingNumber)
+        {
+            //Act
+            var result = MissingNumber.Run(numbers);
+
+            //Assert
+            result.Should().Be(expectedMissingNumber);
+        }
+    }
+}
