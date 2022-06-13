34c22b8633c4a94e5e4db79562970bef8c038063
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-13 19:39:12 +1000

    Graph DeepCopy And Tests

---
+++ Algorithms.Tests/GraphDeepCopyTests.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class GraphDeepCopyTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(Graph graph, Graph expectedCopy)
+        {
+            //Act
+            var copy = graph.DeepCopy();
+
+            //Assert
+            copy.ToString().Should().Be(expectedCopy.ToString());
+
+        }
+
+        public static object[] TestData => new object[]
+        {
+            new object[] { Graph.Generate(), Graph.Generate() }
+        };
+    }
+}
