8dd303ccc06405bfc3c77f3a4bc307e6199ef6ff
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-20 21:49:08 +1000

    Remove first digit if its 5

---
+++ Algorithms.Tests/Remove5Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class Remove5Tests
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(12, 12)]
+        [InlineData(123, 123)]
+        [InlineData(5, 0)]
+        [InlineData(15, 1)]
+        [InlineData(515, 51)]
+        [InlineData(51521, 5121)]
+        public void Test(int number, int expectedResult)
+        {
+            //Act
+            int result = Remove5.Run(number);
+
+            //Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ Algorithms/Remove5.cs
@@ -0,0 +1,39 @@
+﻿namespace Algorithms.Tests
+{
+    public class Remove5
+    {
+        /*
+         * 12534
+         * 1234
+         * 
+         * result: 34
+         * digit: 5
+         * number: 125
+         * power: 100
+         * 
+         * result: 4 
+         * digit: 3
+         * number: 1253
+         * power: 10
+         * 
+         */
+        public static int Run(int number)
+        {
+            int result = 0;
+            int power = 1;
+            while(number > 0)
+            {
+                int digit= number % 10;
+                number /= 10;
+                
+                if (digit == 5)
+                    return number * power + result;
+
+                result = digit * power + result;
+
+                power *= 10;
+            }
+            return result;
+        }
+    }
+}
