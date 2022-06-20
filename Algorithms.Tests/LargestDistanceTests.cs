68e699218dc9dbe297d8290a0443545d52ec039b
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-20 21:34:01 +1000

    Find largest distance between equal items

---
+++ Algorithms.Tests/LargestDistanceTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class LargestDistanceTests
+    {
+        [Theory]
+        [InlineData(new int[] { }, 0)]
+        [InlineData(new [] {1,2,1 }, 2)]
+        [InlineData(new [] {1,2,4,3,4,2,3 }, 4)]
+        public void Test(int[] numbers, int expectedDistance)
+        {
+            //Act
+            int distance = LargestDistance.Run(numbers);
+
+            //Assert
+            distance.Should().Be(expectedDistance);
+        }
+    }
+}
---
+++ Algorithms/LargestDistance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Tests
+{
+    public class LargestDistance
+    {
+        /*
+         * [ 1 2 3 4 2 5 ]
+         *   0 1 2 3 4 5 
+         *   4-1 = 3 
+         */
+        public static int Run(int[] numbers)
+        {
+            int max = 0;
+            var map = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number = numbers[i];
+
+                if (map.ContainsKey(number))
+                    max = Math.Max(max, i - map[number]);
+                else
+                    map.Add(number, i);
+            }
+
+            return max;
+        }
+    }
+}
