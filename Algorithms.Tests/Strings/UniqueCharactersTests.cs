3b390f7f78b80230e5e20311a7ed233fc14cb187
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-22 16:24:17 +1000

    Minimum Deletions for Unique characters

---
+++ Algorithms.Tests/Strings/UniqueCharactersTests.cs
@@ -0,0 +1,27 @@
+﻿using Algorithms.Strings;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Strings
+{
+    public class UniqueCharactersTests
+    {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("a", 0)]
+        [InlineData("ab", 1)]
+        [InlineData("aab", 0)]
+        [InlineData("aabb", 1)]
+        [InlineData("aabbb", 0)]
+        [InlineData("caaabbbc", 2)]
+        [InlineData("ceabaacb", 2)]
+        public void Test(string s, int expectedDeletions)
+        {
+            //Act
+            int deletions = UniqueCharacters.Run(s);
+
+            //Assert
+            deletions.Should().Be(expectedDeletions);
+        }
+    }
+}
