27ab1929851dbb12bf443c36520315c2d862e22c
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-22 19:26:05 +1000

    Min Steps to Make Piles Equal Height

---
+++ Algorithms.Tests/Arrays/MakePilesEqualTests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Arrays;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class MakePilesEqualTests
+    {
+        /*
+         *  5 1 3 4
+         *  5 4 3 1
+         *    1 2 3 = 6 
+         */
+
+        [Theory]
+        [InlineData(new int[] { }, 0)]
+        [InlineData(new[] { 1 }, 0)]
+        [InlineData(new[] { 1, 2 }, 1)]
+        [InlineData(new[] { 5, 2, 1 }, 3)]
+        [InlineData(new[] { 5, 1, 3, 4 }, 6)]
+        public void Test(int[] piles, int expectedSteps)
+        {
+            //Act
+            int steps = MakePilesEqual.Run(piles);
+
+            //Assert
+            steps.Should().Be(expectedSteps);
+        }
+    }
+}
---
+++ Algorithms/Arrays/MakePilesEqual.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Algorithms.Arrays
+{
+    public class MakePilesEqual
+    {
+        /*
+        *  5 1 3 4
+        *  5 4 3 1
+        *    1 2 3   
+        *    1 2 3 = 6 
+        */
+        public static int Run(int[] piles)
+        {
+            piles = piles.OrderByDescending(x => x).ToArray();
+
+            int steps = 0;
+            for (int i = 1; i < piles.Length; i++)
+                if (piles[i] != piles[i - 1])
+                    steps += i;
+         
+            return steps;
+        }
+    }
+}
