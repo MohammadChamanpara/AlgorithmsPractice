a2784b9b0a7af4ff6387cae68a44d726312c5364
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-23 14:21:26 +1000

    Move Left Right in Array to find 0

---
+++ Algorithms.Tests/Arrays/LeftRightTests.cs
@@ -0,0 +1,33 @@
+﻿using Algorithms.Arrays;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class LeftRightTests
+    {
+        /*
+         * Input: arr = [3, 4, 2, 3, 0, 3, 1, 2, 1], start = 7
+         * Output: true
+         * Explanation: left -> left -> right
+         */
+
+        [Theory]
+        [InlineData(new int[] { }, 0, false)]
+        [InlineData(new[] { 1, 2, 3, 0, 1, 7, 5 }, 0, true)]
+        [InlineData(new[] { 1, 2, 3, 0, 1, 7, 5 }, 1, true)]
+        [InlineData(new[] { 1, 2, 3, 0, 1, 7, 5 }, 2, false)]
+        [InlineData(new[] { 1, 2, 3, 0, 1, 7, 5 }, 3, true)]
+        [InlineData(new[] { 1, 2, 3, 0, 1, 7, 5 }, 4, true)]
+        [InlineData(new[] { 1, 2, 3, 0, 1, 7, 5 }, 5, false)]
+        [InlineData(new[] { 1, 2, 3, 0, 1, 7, 5 }, 6, true)]
+        public void Test(int[] numbers, int start, bool expectedOutput)
+        {
+            //Act
+            bool output = LeftRight.Run(numbers, start);
+
+            //Assert
+            output.Should().Be(expectedOutput);
+        }
+    }
+}
