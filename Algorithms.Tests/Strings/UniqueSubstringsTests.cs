681c13baf621a99feba974f7540cb881e7601380
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-23 15:04:29 +1000

    Minimum Unique Substrings

---
+++ Algorithms.Tests/Strings/UniqueSubstringsTests.cs
@@ -0,0 +1,36 @@
+﻿using Algorithms.Strings;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Strings
+{
+    public class UniqueSubstringsTests
+    {
+
+        /*
+         * 
+         * world -> 1
+         * dddd -> 4: d d d d
+         * abba -> 2: ab, ba.
+         * cycle-> 2: cy, cle
+         * 
+         * 
+         * 
+         */
+
+        [Theory]
+        [InlineData("", 1)]
+        [InlineData("world", 1)]
+        [InlineData("dddd", 4)]
+        [InlineData("abba", 2)]
+        [InlineData("cycle", 2)]
+        public void Test(string s, int expectedMinimumSubstrings)
+        {
+            //Act
+            int minimumSubstrings = UniqueSubstrings.Run(s);
+
+            //Assert
+            minimumSubstrings.Should().Be(expectedMinimumSubstrings);
+        }
+    }
+}
---
+++ Algorithms/Strings/UniqueSubstrings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Strings
+{
+    public class UniqueSubstrings
+    {
+        /*
+         * world -> 1
+         * dddd -> 4: d d d d
+         * abba -> 2: ab, ba.
+         * cycle-> 2: cy, cle
+         */
+        public static int Run(string s)
+        {
+            var set = new HashSet<char>();
+            int counter = 1;
+            foreach (char c in s)
+                if (set.Contains(c))
+                {
+                    counter++;
+                    set = new HashSet<char>(new[] { c });
+                }
+                else
+                    set.Add(c);
+
+            return counter;
+        }
+    }
+}
