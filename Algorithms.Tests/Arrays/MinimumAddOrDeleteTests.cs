a8175d287449c38b1612c1776ba306f1f97f4f50
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-23 16:19:15 +1000

    Minimum Add Or Delete

---
+++ Algorithms.Tests/Arrays/MinimumAddOrDeleteTests.cs
@@ -0,0 +1,32 @@
+﻿using Algorithms.Arrays;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class MinimumAddOrDeleteTests
+    {
+        /*
+         * [1,1,1,1,2] --->delete three "1"s and one "2" --> answer should be 4
+         * [1,1,2,2] --> delete one "1" --> answer should be 1
+         * [10,10,10,10,10,10,10] --> add threes "10s" --> answer should be 3
+         * [4,4,4,5,5,2,2] --> add one"4", delete two "5" --> answer should be 3
+         */
+
+        [Theory]
+        [InlineData(new int[] { }, 0)]
+        [InlineData(new[] { 1, 1, 2, 2 }, 1)]
+        [InlineData(new[] { 1, 1, 1, 1, 2 }, 4)]
+        [InlineData(new[] { 4, 4, 4, 5, 5, 2, 2 }, 3)]
+        [InlineData(new[] { 10, 10, 10, 10, 10, 10, 10 }, 3)]
+        public void Test(int[] numbers, int expectedMinimumOperations)
+        {
+            //Act
+            int minimumOperations = MinimumAddOrDelete.Run(numbers);
+
+            //Assert
+            minimumOperations.Should().Be(expectedMinimumOperations);
+
+        }
+    }
+}
---
+++ Algorithms/Arrays/MinimumAddOrDelete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Arrays
+{
+    public static class MinimumAddOrDelete
+    {
+        /*
+         * [1,1,1,1,2] --->delete three "1"s and one "2" --> answer should be 4
+         * [1,1,2,2] --> delete one "1" --> answer should be 1
+         * [10,10,10,10,10,10,10] --> add threes "10s" --> answer should be 3
+         * [4,4,4,5,5,2,2] --> add one"4", delete two "5" --> answer should be 3
+         */
+        public static int Run(int[] numbers)
+        {
+            var counts = new Dictionary<int, int>();
+            foreach (int number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                    counts[number]++;
+                else
+                    counts[number] = 1;
+            }
+
+            int operations = 0;
+            foreach (var x in counts)
+                operations += Math.Min(x.Value, Math.Abs(x.Key - x.Value));
+
+            return operations;
+        }
+    }
+}
