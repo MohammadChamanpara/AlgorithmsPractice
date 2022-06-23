05591f68bd2a94712a3abc3d4b88a59c34d0308e
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-23 15:36:22 +1000

    Greatest Lower and Upper Character

---
+++ Algorithms.Tests/Strings/LowerAndUpperTests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.Strings;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Strings
+{
+    public class LowerAndUpperTests
+    {
+        /*
+         * abnBxcXaA => X
+         * 
+         */
+
+        [Theory]
+        [InlineData("abc", "")]
+        [InlineData("Aab", "A")]
+        [InlineData("AabB", "B")]
+        [InlineData("abnBxcXaA", "X")]
+        public void Test(string s, string expectedLetter)
+        {
+            //Act
+            string letter = LowerAndUpper.Run(s);
+
+            //Assert
+            letter.Should().Be(expectedLetter);
+        }
+    }
+}
