0b9616ce5c17ecfc0c3c4c66dd5c4822a72adf47
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-23 14:51:44 +1000

    Check If Array has all pairs

---
+++ Algorithms.Tests/Arrays/AllPairsTests.cs
@@ -0,0 +1,31 @@
+﻿using Algorithms.Arrays;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class AllPairsTests
+    {
+        /*
+         *  1 2 2 1 3 -> false
+         *  1 2 2 1   -> true
+         */
+
+        [Theory]
+        [InlineData(new int[] { }, true)]
+        [InlineData(new[] { 1 }, false)]
+        [InlineData(new[] { 1, 1 }, true)]
+        [InlineData(new[] { 1, 2 }, false)]
+        [InlineData(new[] { 1, 2, 3 }, false)]
+        [InlineData(new[] { 1, 2, 2, 1 }, true)]
+        [InlineData(new[] { 1, 2, 3, 2, 1 }, false)]
+        public void Test(int[] array, bool expectedAllPairs)
+        {
+            //Act
+            bool allPairs = AllPairs.Run(array);
+
+            //Assert
+            allPairs.Should().Be(expectedAllPairs);
+        }
+    }
+}
