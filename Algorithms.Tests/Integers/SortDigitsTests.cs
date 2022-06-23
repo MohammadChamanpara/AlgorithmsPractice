7b1fad4a8fdbea816e37593f638acc5eaa97cd59
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-23 18:07:17 +1000

    Sort Integer Digits

---
+++ Algorithms.Tests/Integers/SortDigitsTests.cs
@@ -0,0 +1,31 @@
+﻿using Algorithms.Integers;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Integers
+{
+    public class SortDigitsTests
+    {
+        /*
+         *  299 -> 992
+         *  123 -> 321
+         *  1122786-> 8762211
+         */
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(10, 10)]
+        [InlineData(102, 210)]
+        [InlineData(299, 992)]
+        [InlineData(123, 321)]
+        [InlineData(1122786, 8762211)]
+        public void Test(int number, int expectedSorted)
+        {
+            //Act
+            int sorted = SortDigits.Run(number);
+
+            //Assert
+            sorted.Should().Be(expectedSorted);
+        }
+    }
+}
