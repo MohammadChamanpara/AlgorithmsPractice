d18c1d6aba9721d1f16e36f953bd18358f13868d
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-23 11:22:13 +1000

    Generate Unique Arrays

---
+++ Algorithms.Tests/Arrays/GenerateUniqueArrayTests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Arrays;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class GenerateUniqueArrayTests
+    {
+        /*
+         * 4 -> -2,-1,1,2
+         * 5-> -2,-1,0,1,2
+         * 
+         */
+
+        [Theory]
+        [InlineData(1, new[] { 0 })]
+        [InlineData(2, new[] { -1, 1 })]
+        [InlineData(3, new[] { -1, 1, 0 })]
+        [InlineData(4, new[] { -2, -1, 1, 2 })]
+        [InlineData(5, new[] { -2, -1, 1, 2, 0 })]
+        public void Test(int number, int[] expectedArray)
+        {
+            //Act
+            var array = GenerateUniqueArray.Run(number);
+
+            //Assert
+            array.Should().BeEquivalentTo(expectedArray);
+        }
+    }
+}
---
+++ Algorithms/Arrays/GenerateUniqueArray.cs
@@ -0,0 +1,16 @@
+﻿namespace Algorithms.Arrays
+{
+    public class GenerateUniqueArray
+    {
+        public static object Run(int number)
+        {
+            var array = new int[number];
+            for (int i = 0, j = number / 2; i < number / 2; i++, j++)
+            {
+                array[i] = i + 1;
+                array[j] = -(i + 1);
+            }
+            return array;
+        }
+    }
+}
