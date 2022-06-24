7e9b867ab6bbca4aee5685f5bb12b1b8be0f15cd
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-24 11:10:24 +1000

    Binary Gap

---
+++ Algorithms.Tests/Integers/BinaryGapTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Integers
+{
+    public class BinaryGapTests
+    {
+        /*
+         * 100100010
+         * 3
+         * 
+         */
+
+        [Theory]
+        [InlineData(0b0, 0)]
+        [InlineData(0b10, 0)]
+        [InlineData(0b100000000, 0)]
+        [InlineData(0b100100010, 3)]
+        [InlineData(0b100001001, 4)]
+        public void Test(int number, int expectedLongestGap)
+        {
+            //Act
+            int longestGap = BinaryGap.Run(number);
+
+            //Assert
+            longestGap.Should().Be(expectedLongestGap);
+        }
+    }
+}
---
+++ Algorithms/Integers/BinaryGap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Algorithms.Tests.Integers
+{
+    public static class BinaryGap
+    {
+        // 100001001 -> 4
+        public static int Run(int number)
+        {
+            int max = 0;
+            int gap = 0;
+            bool started = false;
+
+            while (number > 0)
+            {
+                if ((number & 1) == 1)
+                {
+                    started = true;
+                    gap = 0;
+                }
+                else if (started)
+                    max = Math.Max(max, ++gap);
+
+                number >>= 1;
+            }
+            return max;
+        }
+    }
+}
