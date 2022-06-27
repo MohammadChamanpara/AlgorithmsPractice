398bc6c3e101d36504f5e1230e8a04ce54f6d573
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-27 13:08:43 +1000

    Largest Rectangle

---
+++ Algorithms.Tests/Arrays/HistogramTests.cs
@@ -0,0 +1,24 @@
+﻿using Algorithms.Arrays;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class HistogramTests
+    {
+
+        [Theory]
+        [InlineData(new[] { 1, 1, 1 }, 3)]
+        [InlineData(new[] { 3, 2, 1 }, 4)]
+        [InlineData(new[] { 1, 10, 1 }, 10)]
+        [InlineData(new[] { 6, 2, 5, 4, 5, 1, 6 }, 12)]
+        public void Test(int[] numbers, int expectedLength)
+        {
+            //Act
+            int len = Histogram.Run(numbers);
+
+            //Assert
+            len.Should().Be(expectedLength);
+        }
+    }
+}
---
+++ Algorithms.Tests/Arrays/LargestRectangleTests.cs
@@ -0,0 +1,60 @@
+﻿using Algorithms.Arrays;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class LargestRectangleTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[,] matrix, int expectedSize)
+        {
+            //Act
+            int size = LargestRectangle.Run(matrix);
+
+            //Assert
+            size.Should().Be(expectedSize);
+        }
+
+        public static object[] TestData => new object[]
+        {
+            new object[]
+            {
+                new [,]    // Input Matrix
+                {
+                    { 0,0,0,0 },
+                    { 0,0,0,0 },
+                    { 0,0,0,0 },
+                    { 0,0,0,0 }
+                },
+                0   //Expected Rectangle Size
+            },
+            
+            new object[]
+            {
+                new [,]    // Input Matrix
+                {
+                    { 0,1,1,0 },
+                    { 1,1,1,1 },
+                    { 1,1,1,1 },
+                    { 1,1,0,0 }
+                },
+                8   //Expected Rectangle Size
+            },
+
+            new object[]
+            {
+                new [,]    // Input Matrix
+                {
+                    { 0,1,0,0 },
+                    { 0,1,1,1 },
+                    { 1,1,1,1 },
+                    { 1,1,0,0 }
+                },
+                6   //Expected Rectangle Size
+            }
+
+        };
+    }
+}
