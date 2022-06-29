71ef6234606113f5534098f2141dfbf476c7e94c
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-29 13:50:24 +1000

    Generate Spiral Matrix

---
+++ Algorithms.Tests/Matrix/MatrixGenerateSpiral.cs
@@ -0,0 +1,49 @@
+﻿namespace Algorithms.Tests.Matrix
+{
+    internal class MatrixGenerateSpiral
+    {
+        internal static int[,] Run(int number)
+        {
+            var matrix = new int[number, number];
+            int fromRow = 0;
+            int toRow = matrix.GetLength(0) - 1;
+            int fromCol = 0;
+            int toCol = matrix.GetLength(1) - 1;
+
+            int direction = 0;
+            int value = 1;
+            while (fromRow <= toRow && fromCol <= toCol)
+            {
+                switch (direction)
+                {
+                    case 0: // ->
+                        for (int col = fromCol; col <= toCol; col++)
+                            matrix[fromRow, col] = value++;
+                        fromRow++;
+                        break;
+
+                    case 1: // V
+                        for (int row = fromRow; row <= toRow; row++)
+                            matrix[row, toCol] = value++;
+                        toCol--;
+                        break;
+
+                    case 2: // <-
+                        for (int col = toCol; col >= fromCol; col--)
+                            matrix[toRow, col] = value++;
+                        toRow--;
+                        break;
+
+                    case 3: // ^
+                        for (int row = toRow; row >= fromRow; row--)
+                            matrix[row, fromCol] = value++;
+                        fromCol++;
+                        break;
+                }
+                direction++;
+                direction %= 4;
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Algorithms.Tests/Matrix/MatrixGenerateSpiralTests.cs
@@ -0,0 +1,52 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Matrix
+{
+    public class MatrixGenerateSpiralTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int number, int[,] expectedMatrix)
+        {
+            //Act
+            int[,] matrix = MatrixGenerateSpiral.Run(number);
+
+            //Assert
+            matrix.Should().BeEquivalentTo(expectedMatrix);
+        }
+
+        public static object[] TestData => new object[]
+        {
+            new object[]
+            {
+                1,
+                new[,]
+                {
+                    { 1 }
+                }
+            },
+
+            new object[]
+            {
+                2,
+                new[,]
+                {
+                    { 1, 2 },
+                    { 4, 3 }
+                }
+            },
+
+            new object[]
+            {
+                3,
+                new[,]
+                {
+                    { 1, 2, 3 },
+                    { 8, 9, 4 },
+                    { 7, 6, 5 }
+                }
+            }
+        };
+    }
+}
