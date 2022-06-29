40ce933eeaa5569169415cafb875841f82c6c72f
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-29 12:42:58 +1000

    Find Duplicate Array element at Time O(1) and Space <O(n)

---
+++ Algorithms.Tests/Arrays/RepeatedElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Tests.Arrays
+{
+    internal class RepeatedElement
+    {
+        internal static int Run(int[] numbers)
+        {
+            var range = FindRange(numbers);
+
+            return range.HasValue
+                ? FindDuplicate(numbers, range.Value)
+                : -1;
+        }
+
+        private static int FindDuplicate(int[] numbers, int range)
+        {
+            var set = new HashSet<int>();
+            foreach (var number in numbers)
+            {
+                int sqrt = (int)Math.Sqrt(number);
+
+                if (sqrt != range)
+                    continue;
+
+                if (!set.Add(number))
+                    return number;
+            }
+            return -1;
+        }
+
+        private static int? FindRange(int[] numbers)
+        {
+            var set = new HashSet<int>();
+            foreach (var number in numbers)
+            {
+                int sqrt = (int)Math.Sqrt(number);
+                if (!set.Add(sqrt))
+                    return sqrt;
+            }
+            return null;
+        }
+    }
+}
---
+++ Algorithms.Tests/Arrays/RepeatedElementTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class RepeatedElementTests
+    {
+        [Theory]
+        [InlineData(new int[] { }, -1)]
+        [InlineData(new[] { 1, 2, 2 }, 2)]
+        [InlineData(new[] { 3, 2, 3, 4 }, 3)]
+        [InlineData(new[] { 1, 2, 3, 4 }, -1)]
+        public void Test(int[] numbers, int expectedDuplicate)
+        {
+            //Act
+            int output = RepeatedElement.Run(numbers);
+
+            //Assert
+            output.Should().Be(expectedDuplicate);
+        }
+    }
+}
