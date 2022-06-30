4abe4673c6f510c248890dfb6545aa02dfddbfea
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-30 18:21:04 +1000

    Find First Missing Integer at Time:O(n) Space:O(1)

---
+++ Algorithms.Tests/Arrays/FirstMissingInteger.cs
@@ -0,0 +1,35 @@
+﻿namespace Algorithms.Tests.Arrays
+{
+    internal class FirstMissingInteger
+    {
+        /*
+        
+        [3,4,-1,1] -> 2,
+        [1,2,0] -> 3,
+        [2,3,10] -> 1 
+
+        */
+        internal static int Run(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+                MonkeyJump(numbers, numbers[i] - 1);
+
+            for (int i = 0; i < numbers.Length; i++)
+                if (numbers[i] != i + 1)
+                    return i + 1;
+
+            return numbers.Length + 1;
+        }
+
+        private static void MonkeyJump(int[] numbers, int index)
+        {
+            while (index >= 0 && index < numbers.Length)
+            {
+                if (numbers[index] == index + 1)
+                    return;
+
+                (index, numbers[index]) = (numbers[index] - 1, index + 1);
+            }
+        }
+    }
+}
---
+++ Algorithms.Tests/Arrays/FirstMissingIntegerTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class FirstMissingIntegerTests
+    {
+        [Theory]
+        [InlineData(new int[] { }, 1)]
+        [InlineData(new[] { 0 }, 1)]
+        [InlineData(new[] { -1 }, 1)]
+        [InlineData(new[] { 1, 2 }, 3)]
+        [InlineData(new[] { -1, 2 }, 1)]
+        [InlineData(new[] { 0, 0, 0, }, 1)]
+        [InlineData(new[] { -1, -2, 1 }, 2)]
+        [InlineData(new[] { -1, -2, -3, }, 1)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 6)]
+        [InlineData(new[] { -1, -2, 1, 0, 0, 2, 4, 5, 6, 7, 8 }, 3)]
+        public void Test(int[] numbers, int expectedResult)
+        {
+            //Act
+            int result = FirstMissingInteger.Run(numbers);
+
+            //Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
