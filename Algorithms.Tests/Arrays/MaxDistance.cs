4e9984fca26188379a3334ab87e22ad91ee18d1f
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-07-01 07:41:10 +1000

    Max Distance in array

---
+++ Algorithms.Tests/Arrays/MaxDistance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Algorithms.Tests.Arrays
+{
+    internal class MaxDistance
+    {
+        internal static int Run(int[] numbers)
+        {
+            if (numbers.Length == 0) 
+                return -1;
+
+            int[] min = new int[numbers.Length];
+            int[] max = new int[numbers.Length];
+
+            min[0] = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+                min[i] = Math.Min(numbers[i], min[i - 1]);
+
+            max[^1] = numbers[^1];
+            for (int i = numbers.Length - 2; i >= 0; i--)
+                max[i] = Math.Max(numbers[i], max[i + 1]);
+
+            int left = 0, right = 0;
+            int maxDifference = -1;
+            while (left < numbers.Length && right < numbers.Length)
+            {
+                if (min[left] <= max[right])
+                {
+                    maxDifference = Math.Max(maxDifference, right - left);
+                    right++;
+                }
+                else
+                    left++;
+            }
+
+            return maxDifference;
+        }
+    }
+}
---
+++ Algorithms.Tests/Arrays/MaxDistanceTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class MaxDistanceTests
+    {
+        [Theory]
+        [InlineData(new int[] { }, -1)]
+        [InlineData(new[] { 2, 1, 3 }, 2)]
+        [InlineData(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 10 }, 9)]
+        [InlineData(new[] { 9, 10, 7, 6, 5, 4, 3, 2, 1, 8 }, 7)]
+
+        public void Test(int[] numbers, int expectedDistance)
+        {
+            //Act
+            int distance = MaxDistance.Run(numbers);
+
+            //Assert
+            distance.Should().Be(expectedDistance);
+        }
+    }
+}
