f1cb9855d6775f44349d707721c97e9f84d59e28
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-30 20:06:35 +1000

    Add One to number as Array

---
+++ Algorithms.Tests/Arrays/AddOne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Tests.Arrays
+{
+    internal class AddOne
+    {
+        /*
+        { 1 },       -> { 2 })]
+        { 1, 2 },    -> { 1, 3 })]
+        { 1, 2, 9 }, -> { 1, 3, 0 })]
+        { 0, 0, 1 }, -> { 2 })]
+         */
+        internal static int[] Run(int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            if (array.Length == 0)
+                return new[] { 0 };
+
+            var list = new List<int>();
+
+            array[^1]++;
+            int carry = 0;
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                int digit = array[i] + carry;
+                carry = digit / 10;
+                if (digit == 0)
+                    break;
+                list.Add(digit %= 10);
+            }
+            if (carry > 0)
+                list.Add(carry);
+
+            list.Reverse();
+            return list.ToArray();
+        }
+    }
+}
---
+++ Algorithms.Tests/Arrays/AddOneTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class AddOneTests
+    {
+        [Theory]
+        [InlineData(new[] { 0 }, new[] { 1 })]
+        [InlineData(new[] { 1 }, new[] { 2 })]
+        [InlineData(new[] { 9 }, new[] { 1, 0 })]
+        [InlineData(new[] { 1, 2 }, new[] { 1, 3 })]
+        [InlineData(new[] { 1, 2, 9 }, new[] { 1, 3, 0 })]
+        [InlineData(new[] { 0, 0, 1 }, new[] { 2 })]
+        [InlineData(new[] { 9, 9, 9 }, new[] { 1, 0, 0, 0 })]
+        [InlineData(new[] { 0, 0, 0, 0, 0, 9, 9, 9 }, new[] { 1, 0, 0, 0 })]
+        public void Test(int[] array, int[] expectedResult)
+        {
+            //Act
+            int[] result = AddOne.Run(array);
+
+            //Assert
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
