ac1f8928400bb436d56cb2578bff779e599eb0cb
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-06-30 13:21:01 +1000

    Build Largest number by concatenating ints

---
+++ Algorithms.Tests/Integers/LargestNumber.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Tests.Integers
+{
+    internal static class LargestNumber
+    {
+        internal static string Run(int[] numbers)
+        {
+            var list = numbers.ToList();
+            list.Sort((x, y) => Compare(y, x));
+            return string.Join(separator: "", list);
+        }
+
+        /// <summary>
+        /// 9 > 123
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static int Compare(int x, int y)
+        {
+            int[] xDigits = x.ToArray();
+            int[] yDigits = y.ToArray();
+
+            for (int i = 0; i < xDigits.Length; i++)
+            {
+                if (i == yDigits.Length)
+                    return -1;
+
+                if (xDigits[i] > yDigits[i])
+                    return 1;
+
+                if (xDigits[i] < yDigits[i])
+                    return -1;
+            }
+
+            if (xDigits.Length < yDigits.Length)
+                return 1;
+
+            return 0;
+        }
+
+        /// <summary>
+        /// 123 -> [1,2,3]
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        private static int[] ToArray(this int number)
+        {
+            var list = new List<int>();
+            while (number > 0)
+            {
+                list.Add(number % 10);
+                number /= 10;
+            }
+            list.Reverse();
+            return list.ToArray();
+        }
+    }
+}
---
+++ Algorithms.Tests/Integers/LargestNumberTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Integers
+{
+    public class LargestNumberTests
+    {
+        /*
+         *  [ 9, 32, 31, 832, 84, 5]
+         *  9 84 832 5 32 31
+         *
+         *
+         */
+
+        [Theory]
+        [InlineData(new[] { 1 }, "1")]
+        [InlineData(new[] { 2, 3 }, "32")]
+        [InlineData(new[] { 22, 23 }, "2322")]
+        [InlineData(new[] { 22, 23, 5, 51 }, "5512322")]
+        [InlineData(new[] { 9, 32, 31, 832, 84, 5 }, "98483253231")]
+        public void Test(int[] numbers, string expectedLargest)
+        {
+            //Act
+            string largest = LargestNumber.Run(numbers);
+
+            //Assert
+            largest.Should().Be(expectedLargest);
+        }
+    }
+}
