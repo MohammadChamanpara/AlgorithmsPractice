37bf6c33ae1163a1c6b841fa6708e4edb62b30c6
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-07-01 11:32:00 +1000

    Next Permutation of Array Numbers

---
+++ Algorithms.Tests/Arrays/NextPermutation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms.Tests.Arrays
+{
+    internal class NextPermutation
+    {
+        internal static int[] Run(int[] array)
+        {
+            var startPosition = Swap(array);
+            Array.Reverse(array, startPosition, array.Length - startPosition);
+            return array;
+        }
+
+        private static int Swap(int[] array)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+                if (array[i] > array[i - 1])
+                {
+                    (array[i], array[i - 1]) = (array[i - 1], array[i]);
+                    return i + 1;
+                }
+
+            return 0;
+        }
+    }
+}
---
+++ Algorithms.Tests/Arrays/NextPermutationTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using FluentAssertions.Equivalency;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class NextPermutationTests
+    {
+        [Theory]
+        [InlineData(new[] { 0 }, new[] { 0 })]
+        [InlineData(new[] { 1 }, new[] { 1 })]
+        [InlineData(new[] { 1, 2, 3 }, new[] { 1, 3, 2 })]
+        [InlineData(new[] { 3, 2, 1 }, new[] { 1, 2, 3 })]
+        [InlineData(new[] { 1, 1, 5 }, new[] { 1, 5, 1 })]
+        [InlineData(new[] { 4, 3, 2, 1 }, new[] { 1, 2, 3, 4 })]
+        [InlineData(new[] { 3, 4, 2, 1 }, new[] { 4, 3, 1, 2 })]
+        [InlineData(new[] { 20, 50, 113 }, new[] { 20, 113, 50 })]
+        public void Test(int[] array, int[] expectedResult)
+        {
+            //Act
+            int[] result = NextPermutation.Run(array);
+
+            //Assert
+            result.Should().BeEquivalentTo(expectedResult, options => options.WithStrictOrdering());
+        }
+    }
+}
