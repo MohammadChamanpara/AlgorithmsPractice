7c2705f32cbe6756f1c022b62fc0d5b52b1cef36
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-07-03 12:34:31 +1000

    Next Permutation Improvements

--- Algorithms.Tests/Arrays/NextPermutation.cs
+++ Algorithms.Tests/Arrays/NextPermutation.cs
@@ -1,27 +1,50 @@
 ﻿using System;
-using System.Linq;
 
 namespace Algorithms.Tests.Arrays
 {
     internal class NextPermutation
     {
-        internal static int[] Run(int[] array)
+        internal static int[] Run(int[] numbers)
         {
-            var startPosition = Swap(array);
-            Array.Reverse(array, startPosition, array.Length - startPosition);
-            return array;
+            var changingDigitIndex = FindChangingDigit(numbers);
+            if (changingDigitIndex >= 0)
+            {
+                var replacingDigitIndex = FindMinIndex(numbers, changingDigitIndex + 1);
+                Swap(numbers, changingDigitIndex, replacingDigitIndex);
+            }
+
+            var start = changingDigitIndex + 1;
+            Array.Reverse(numbers, start, numbers.Length - start);
+            return numbers;
         }
 
-        private static int Swap(int[] array)
+        private static void Swap(int[] numbers, int i, int j)
         {
-            for (int i = array.Length - 1; i > 0; i--)
-                if (array[i] > array[i - 1])
+            (numbers[i], numbers[j]) = (numbers[j], numbers[i]);
+        }
+
+        private static int FindMinIndex(int[] numbers, int start)
+        {
+            int limit = numbers[start - 1];
+            int min = numbers[start];
+            int minIndex = start;
+            for (int i = start + 1; i < numbers.Length; i++)
+                if (numbers[i] > limit && numbers[i] < min)
                 {
-                    (array[i], array[i - 1]) = (array[i - 1], array[i]);
-                    return i + 1;
+                    min = numbers[i];
+                    minIndex = i;
                 }
 
-            return 0;
+            return minIndex;
+        }
+
+        private static int FindChangingDigit(int[] array)
+        {
+            for (int i = array.Length - 2; i >= 0; i--)
+                if (array[i] < array[i + 1])
+                    return i;
+
+            return -1;
         }
     }
 }
--- Algorithms.Tests/Arrays/NextPermutationTests.cs
+++ Algorithms.Tests/Arrays/NextPermutationTests.cs
@@ -13,8 +13,13 @@ namespace Algorithms.Tests.Arrays
         [InlineData(new[] { 3, 2, 1 }, new[] { 1, 2, 3 })]
         [InlineData(new[] { 1, 1, 5 }, new[] { 1, 5, 1 })]
         [InlineData(new[] { 4, 3, 2, 1 }, new[] { 1, 2, 3, 4 })]
-        [InlineData(new[] { 3, 4, 2, 1 }, new[] { 4, 3, 1, 2 })]
+        [InlineData(new[] { 3, 4, 2, 1 }, new[] { 4, 1, 2, 3 })]
         [InlineData(new[] { 20, 50, 113 }, new[] { 20, 113, 50 })]
+        [InlineData(new[] { 59, 854, 422 }, new[] { 422, 59, 854 })]
+        [InlineData(new[] { 1, 3, 2 }, new[] { 2, 1, 3 })]
+        [InlineData(new[] { 3, 5, 4 }, new[] { 4, 3, 5 })]
+        [InlineData(new[] { 1, 3, 8, 7, 6, 5, 4, 2 }, new[] { 1, 4, 2, 3, 5, 6, 7, 8 })]
+
         public void Test(int[] array, int[] expectedResult)
         {
             //Act
