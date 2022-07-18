269728d6b28e125448612793af7b06f37cd822c6
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-07-18 15:50:50 +1000

    Minimum Unsorted Subarray

---
+++ Algorithms.Tests/Arrays/MinimumUnsortedSubArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Algorithms.Tests.Arrays
+{
+    internal class MinimumUnsortedSubArray
+    {
+        internal static int[] Run(int[] array)
+        {
+            // 1 10 15 8 20
+            // 1, 3, 2, 4, 5
+
+            int[] maxSoFar = new int[array.Length];
+            maxSoFar[0] = array[0];
+            for (int i = 1; i < array.Length; i++)
+                maxSoFar[i] = Math.Max(array[i], maxSoFar[i - 1]);
+
+            int[] minToEnd = new int[array.Length];
+            minToEnd[array.Length - 1] = array[array.Length - 1];
+            for (int i = array.Length - 2; i >= 0; i--)
+                minToEnd[i] = Math.Min(array[i], minToEnd[i + 1]);
+
+            int start = -1;
+            while (++start < array.Length && array[start] <= minToEnd[start]) ;
+
+            if (start == array.Length)
+                return new[] { -1 };
+
+            int end = array.Length;
+            while (array[--end] >= maxSoFar[end]) ;
+
+            return new[] { start, end };
+        }
+    }
+}
---
+++ Algorithms.Tests/Arrays/MinimumUnsortedSubArrayTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class MinimumUnsortedSubArrayTests
+    {
+        [Theory]
+        [InlineData(new[] { 1 }, new[] { -1 })]
+        [InlineData(new[] { 1, 2 }, new[] { -1 })]
+        [InlineData(new[] { 1, 3, 2 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 3, 2, 4 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 3, 2, 4, 5 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 1, 10, 10, 15, 10, 15, 10, 10, 15, 10, 15 }, new[] { 4, 10 })]
+        [InlineData(new[] { 1, 2, 3, 5, 6, 13, 15, 16, 17, 13, 13, 15, 17, 17, 17, 17, 17, 19, 19 }, new[] { 6, 11 })]
+        public void Test(int[] array, int[] expectedResult)
+        {
+            //Act
+            int[] result = MinimumUnsortedSubArray.Run(array);
+
+            //Assert
+            result.Should().BeEquivalentTo(expectedResult, options => options.WithoutStrictOrdering());
+        }
+    }
+}
