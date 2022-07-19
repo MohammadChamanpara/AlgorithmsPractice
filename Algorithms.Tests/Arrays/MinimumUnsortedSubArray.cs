3914bd7e435bfc866c1173206c680f14493f7962
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2022-07-19 15:35:46 +1000

    Minimum Unsorted Subarray With O(1) Sapce

--- Algorithms.Tests/Arrays/MinimumUnsortedSubArray.cs
+++ Algorithms.Tests/Arrays/MinimumUnsortedSubArray.cs
@@ -3,11 +3,70 @@ namespace Algorithms.Tests.Arrays
 {
     internal class MinimumUnsortedSubArray
     {
-        internal static int[] Run(int[] array)
+        internal static int[] Run(int[] array) //O(1) Space
         {
-            // 1 10 15 8 20
-            // 1, 3, 2, 4, 5
+            // 1 5 10 2 3 20 15 16 18 21 22
+            //     ^         ^
+            //   ^                  ^
 
+
+            //Find Start
+            int start = FindStart(array);
+            if (start == -1)
+                return new[] { -1 };
+
+            //Find End 
+            int end = FindEnd(array);
+
+
+            //Find min and max
+            (int Min, int Max) = FindMinMax(array, start, end);
+
+
+            //Bring back start
+            start = -1;
+            while (array[++start] <= Min) ;
+
+
+            //Push forward end
+            end = array.Length;
+            while (array[--end] >= Max) ;
+
+            return new[] { start, end };
+        }
+
+        private static (int Min, int Max) FindMinMax(int[] array, int start, int end)
+        {
+            int min, max;
+            min = max = array[start];
+            for (int i = start + 1; i <= end; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+                else if (array[i] > max)
+                    max = array[i];
+            }
+            return (min, max);
+        }
+
+        private static int FindStart(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+                if (array[i] < array[i - 1])
+                    return i - 1;
+            return -1;
+        }
+
+        private static int FindEnd(int[] array)
+        {
+            for (int i = array.Length - 2; i >= 0; i--)
+                if (array[i] > array[i + 1])
+                    return i + 1;
+            return -1;
+        }
+
+        internal static int[] Run2(int[] array)  //O(n) Space
+        {
             int[] maxSoFar = new int[array.Length];
             maxSoFar[0] = array[0];
             for (int i = 1; i < array.Length; i++)
--- Algorithms.Tests/Arrays/MinimumUnsortedSubArrayTests.cs
+++ Algorithms.Tests/Arrays/MinimumUnsortedSubArrayTests.cs
@@ -8,11 +8,13 @@ namespace Algorithms.Tests.Arrays
         [Theory]
         [InlineData(new[] { 1 }, new[] { -1 })]
         [InlineData(new[] { 1, 2 }, new[] { -1 })]
+        [InlineData(new[] { 2, 1 }, new[] { 0, 1 })]
         [InlineData(new[] { 1, 3, 2 }, new[] { 1, 2 })]
         [InlineData(new[] { 1, 3, 2, 4 }, new[] { 1, 2 })]
         [InlineData(new[] { 1, 3, 2, 4, 5 }, new[] { 1, 2 })]
         [InlineData(new[] { 1, 1, 10, 10, 15, 10, 15, 10, 10, 15, 10, 15 }, new[] { 4, 10 })]
         [InlineData(new[] { 1, 2, 3, 5, 6, 13, 15, 16, 17, 13, 13, 15, 17, 17, 17, 17, 17, 19, 19 }, new[] { 6, 11 })]
+        [InlineData(new[] { 1, 2, 3, 3, 4, 4, 5, 5, 7, 8, 9, 20, 14, 16, 19, 14, 19, 19, 20 }, new[] { 11,17 })]
         public void Test(int[] array, int[] expectedResult)
         {
             //Act
